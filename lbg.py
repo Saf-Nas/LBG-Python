d17235be3cb976704b26eae2ae97eb19c0c28ec3
Saf-Nas <107881835+Saf-Nas@users.noreply.github.com>
2023-05-26 13:37:36 +0100

    Update lbg.py

--- lbg.py
+++ lbg.py
@@ -209,13 +209,14 @@ def delete_one(_id):
 # module import protection
 if __name__ == '__main__':
     # get app to serve
-    print(f'API Listening on http://{HOST}:{PORT}')
-    app.run(host=HOST, port=PORT, debug=True)
-
-    # set up the app with listening socket for http requests and appropriate hostname
     parser = argparse.ArgumentParser()
     parser.add_argument("--PORT", default="5000")
     args = parser.parse_args()
 
     PORT = args.PORT
     HOST = '0.0.0.0'
+    print(f'API Listening on http://{HOST}:{PORT}')
+    app.run(host=HOST, port=PORT, debug=True)
+
+    # set up the app with listening socket for http requests and appropriate hostname
+   
